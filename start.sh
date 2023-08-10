7ae772923aa4c697de8eb7bfc62d0fc7fd1bbc99
parassirohi <parassirohi000@gmail.com>
2023-08-10 14:55:35 +0530

    adding superuser for render

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 python manage.py migrate &
-python manage.py runserver 0.0.0.0:8000 && python manage.py add_user
+python manage.py add_user &
+python manage.py runserver 0.0.0.0:8000 
 
